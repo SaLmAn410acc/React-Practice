e4df4a34474257dcfdbcea178912063063b33b14
Salman <muhammadsalman410@gmail.com>
2023-09-07 22:23:36 +0500

    learn useReducer

--- react-hooks/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ react-hooks/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,30 +1,73 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useReducer } from "react";
+import { data } from "../../../data";
+
+const reducer = (state, action) => {
+  if (action.type === "remove_id") {
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload
+    );
+    return { ...state, people: newPeople };
+  }
+  if (action.type === "clear_list") {
+    return { ...state, people: [] };
+  }
+  if (action.type === "reset_list") {
+    return { ...state, people: data };
+  }
+};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  let initialState = {
+    people: data,
+  };
+
+  // const [people, setPeople] = React.useState(data);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    console.log(id);
+    dispatch({ type: "remove_id", payload: id });
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
   };
+
+  const clearItems = () => {
+    dispatch({ type: "clear_list" });
+  };
+
+  const resetList = () => {
+    dispatch({ type: "reset_list" });
+  };
+
   return (
     <div>
-      {people.map((person) => {
-        const { id, name } = person;
+      {state.people.map((user) => {
+        const { id, name } = user;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {state.people.length >= 1 ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearItems}
+        >
+          clear items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetList}
+        >
+          reset list
+        </button>
+      )}
     </div>
   );
 };
