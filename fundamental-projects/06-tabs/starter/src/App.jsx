49f14b62997bbfa49ddb8b8ce9ae9fecd18575b6
Salman <muhammadsalman410@gmail.com>
2023-09-16 14:42:35 +0500

    commiting

--- fundamental-projects/06-tabs/starter/src/App.jsx
+++ fundamental-projects/06-tabs/starter/src/App.jsx
@@ -1,6 +1,70 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    fetchData(url);
+  }, []);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      // console.log(data);
+      setJobs(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(true);
+    }
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  const { title, company, dates, duties } = jobs[value];
+
+  return (
+    <section className="jobs-center">
+      {/* btn container */}
+      <div className="btn-container">
+        {jobs.map((job, index) => {
+          return (
+            <button
+              key={index}
+              className={`job-btn ${index === value && "active-btn"}`}
+              onClick={() => setValue(index)}
+            >
+              {job.company}
+            </button>
+          );
+        })}
+      </div>
+      {/* job info */}
+      <article className="job-info">
+        <h3>{title}</h3>
+        <span className="job-company">{company}</span>
+        <p className="job-date">{dates}</p>
+
+        <div>
+          {duties.map((duty, index) => {
+            return (
+              <div className="job-desc" key={index}>
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </div>
+      </article>
+    </section>
+  );
 };
 export default App;
--- fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+
+import data from "./data";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(count);
+
+    if (amount < 0) {
+      amount = 1;
+    }
+    if (amount > 8) {
+      amount = 8;
+    }
+
+    setText(data.splice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input value={count} onChange={(e) => setCount(e.target.value)} />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((txt, index) => {
+          return <p key={index}>{txt}</p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
